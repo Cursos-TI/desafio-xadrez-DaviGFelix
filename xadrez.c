213a9d350046f5ea8bb9c8a6b27548992d5529f6
DaviGFelix <daviguimafelix@gmail.com>
2025-09-18 20:06:26 +0000

    finalizei o avançado

--- xadrez.c
+++ xadrez.c
@@ -3,18 +3,17 @@
 int main() {
     // Simulação do Movimento da Torre (usando for)
     // A Torre se move 5 casas para a direita.
-    printf("Movimento da Torre\n");
+    printf("Movimento da Torre... \n");
     int casas_torre = 5;
     for (int i = 0; i < casas_torre; i++) {
         printf("Direita\n");
     }
 
-    // Separador para a próxima simulação
     printf("\n");
 
     // Simulação do Movimento do Bispo (usando while)
     // O Bispo se move 5 casas na diagonal (para cima e para a direita).
-    printf("Movimento do Bispo\n");
+    printf("Movimento do Bispo...\n");
     int casas_bispo = 5;
     int contador_bispo = 0;
     while (contador_bispo < casas_bispo) {
@@ -22,12 +21,11 @@ int main() {
         contador_bispo++;
     }
 
-    // Separador para a próxima simulação
     printf("\n");
 
     // Simulação do Movimento da Rainha (usando do-while)
     // A Rainha se move 8 casas para a esquerda.
-    printf("Movimento da Rainha\n");
+    printf("Movimento da Rainha...\n");
     int casas_rainha = 8;
     int contador_rainha = 0;
     if (casas_rainha > 0) {
@@ -35,6 +33,27 @@ int main() {
             printf("Esquerda\n");
             contador_rainha++;
         } while (contador_rainha < casas_rainha);
+    }
+
+    printf("\n");
+
+    // Simulação do Movimento do Cavalo (usando loops aninhados)
+    // O Cavalo se move duas casas para baixo e uma para a esquerda.
+    printf("Movimento do Cavalo...\n");
+
+    // O loop for simula as 2 casas para baixo.
+    // O loop externo gerencia o primeiro segmento do movimento em "L".
+    int movimento_vertical = 2;
+    for (int i = 0; i < movimento_vertical; i++) {
+        printf("Baixo\n");
+    }
+    
+    
+    // O loop interno gerencia o segundo segmento perpendicular do movimento.
+    int movimento_horizontal = 1;
+    while (movimento_horizontal > 0) {
+        printf("Esquerda\n");
+        movimento_horizontal--;
     }
 
     return 0;
